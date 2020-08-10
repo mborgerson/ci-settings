6661b9bb4b9851da53519d2961d7bf0ab6c29d1c
Kevin Phoenix <kevin@kphoenix.us>
2020-08-10 16:06:29 -0700

    Checkout commits before making release commits

---
+++ scripts/checkout_all.py
@@ -0,0 +1,27 @@
+import subprocess
+
+import yaml
+
+from repos import parse_repos, REPOS_CONFIG
+
+
+def checkout_repo(repo, commit):
+    subprocess.run(f"git clone git@github.com:{repo.repo}.git {repo.name}".split())
+    subprocess.run(f"git -C {repo.name} checkout {commit}")
+
+
+def parse_commits(path):
+    with open(path, "r") as f:
+        return yaml.load(f.read(), loader=yaml.Loader)
+
+
+def main():
+    repos = parse_repos(REPOS_CONFIG)
+    commits = parse_commits("versions.yaml")
+
+    for repo in repos:
+        checkout_repo(repo, commit[repo.name])
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/repos.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+import yaml
+
+REPOS_CONFIG = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "..", "repos.yaml"
+)
+
+
+@dataclass
+class Repo:
+    name: str
+    repo: str
+
+
+def parse_repos(path):
+    with open(path, "r") as f:
+        raw = yaml.load(f, Loader=yaml.Loader)
+    return [Repo(**r) for r in raw["repos"]]
