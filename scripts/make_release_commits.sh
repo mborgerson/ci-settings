bafedd1eefbb1c250f5ece1b4a8117fde3bb3f73
Kevin Phoenix <kevin@kphoenix.us>
2020-08-10 15:35:30 -0700

    Add create release commits stage for release

---
+++ scripts/make_release_commits.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+VERSION_MAJOR=8
+VERSION=$VERSION_MAJOR$(date +.%y.%m.%d | sed -e "s/\.0*/./g")
+
+REPOS=$@
+
+for i in $REPOS; do
+    if [ ! -e $i/setup.py ]; then
+        continue
+    fi
+
+    # Replace version in setup.py
+    sed -i -e "s/version=['\"][^'\"]*['\"]/version='$VERSION'/g" $i/setup.py
+    # Replace version in __init__.py
+    sed -i -e "s/^__version__ = .*/__version__ = $(version_to_tuple $VERSION)/g" $i/*/__init__.py
+
+    for j in $REPOS; do
+        if [ "$i" == "$j" ]; then
+            continue;
+        fi
+
+        # Replace dependency versions in setup.py
+        sed -i -e "s/'$j\(\(==[^']*\)\?\)',\$/'$j==$VERSION',/" $i/setup.py
+    done
+done
+
+# Update angr-doc versions
+sed -i -e "s/version = u['\"][^'\"]*['\"]/version = u'$VERSION'/g" angr-doc/api-doc/source/conf.py
+sed -i -e "s/release = u['\"][^'\"]*['\"]/release = u'$VERSION'/g" angr-doc/api-doc/source/conf.py
+
+# Commit and push to github
+git config --global user.name "angr release bot"
+git config --global user.email "angr@lists.cs.ucsb.edu"
+for i in $REPOS; do
+    git checkout -b release/$VERSION
+    git -c $i commit -m "Update version to $VERSION"
+    git tag -a v$VERSION -m "release version $VERSION"
+    git push origin release/$VERSION
+    git push origin v$VERSION
+done
+
+# Create source distributions
+for i in $REPOS; do
+    python $i/setup.py sdist -d sdist
+done
