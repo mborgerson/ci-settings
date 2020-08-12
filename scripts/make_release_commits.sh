2bfd001a378d5cb014360903401e915ca142beb2
Kevin Phoenix <kevin@kphoenix.us>
2020-08-12 16:04:58 -0700

    Refactor make_release_commits.sh to not die on vex repo

--- scripts/make_release_commits.sh
+++ scripts/make_release_commits.sh
@@ -1,62 +1,56 @@
 #!/bin/bash
-
-set -e
-set -x
+set -ex
 
 VERSION_MAJOR=8
 VERSION=$VERSION_MAJOR$(date +.%y.%m.%d | sed -e "s/\.0*/./g")
 VERSION_TUPLE=$(awk -F '.' '{ printf "(%d, %d, %d, %d)\n", $1, $2, $3, $4}' <<< $VERSION)
-
 REPOS=$(python scripts/get_repo_names.py)
-
 CHECKOUT_DIR=repos
+
+git config --global user.name "angr release bot"
+git config --global user.email "angr@lists.cs.ucsb.edu"
 
 for i in $REPOS; do
     pushd $CHECKOUT_DIR/$i
 
-    if [ ! -e setup.py ]; then
+    if [ -e setup.py ]; then
+        # Replace version in setup.py
+        sed -i -e "s/version=['\"][^'\"]*['\"]/version='$VERSION'/g" setup.py
+        # Replace version in __init__.py
+        sed -i -e "s/^__version__ = .*/__version__ = $VERSION_TUPLE/g" ./*/__init__.py
+
+        for j in $REPOS; do
+            if [ "$i" == "$j" ]; then
+                continue;
+            fi
+
+            # Replace dependency versions in setup.py
+            sed -i -e "s/'$j\(\(==[^']*\)\?\)',\$/'$j==$VERSION',/" setup.py
+        done
+        # continue
+    elif [ $i == "angr-doc" ]; then
+        sed -i -e "s/version = u['\"][^'\"]*['\"]/version = u'$VERSION'/g" api-doc/source/conf.py
+        sed -i -e "s/release = u['\"][^'\"]*['\"]/release = u'$VERSION'/g" api-doc/source/conf.py
+    else
         popd
         continue
     fi
 
-    # Replace version in setup.py
-    sed -i -e "s/version=['\"][^'\"]*['\"]/version='$VERSION'/g" setup.py
-    # Replace version in __init__.py
-    sed -i -e "s/^__version__ = .*/__version__ = $VERSION_TUPLE/g" ./*/__init__.py
-
-    for j in $REPOS; do
-        if [ "$i" == "$j" ]; then
-            continue;
-        fi
-
-        # Replace dependency versions in setup.py
-        sed -i -e "s/'$j\(\(==[^']*\)\?\)',\$/'$j==$VERSION',/" setup.py
-    done
-
-    popd
-done
-
-# Update angr-doc versions
-sed -i -e "s/version = u['\"][^'\"]*['\"]/version = u'$VERSION'/g" $CHECKOUT_DIR/angr-doc/api-doc/source/conf.py
-sed -i -e "s/release = u['\"][^'\"]*['\"]/release = u'$VERSION'/g" $CHECKOUT_DIR/angr-doc/api-doc/source/conf.py
-
-# Commit and push to github
-git config --global user.name "angr release bot"
-git config --global user.email "angr@lists.cs.ucsb.edu"
-for i in $REPOS; do
-    pushd $CHECKOUT_DIR/$i
-
+    # Commit and push to github
     git checkout -b release/$VERSION
     git add --all
     git commit -m "Update version to $VERSION"
     git tag -a v$VERSION -m "release version $VERSION"
-    if ! $DRY_RUN; then
-        git push origin release/$VERSION
-        git push origin v$VERSION
-    fi
-
     popd
 done
+
+# Push to github
+if ! $DRY_RUN; then
+    for i in $REPOS; do
+        git -C $CHECKOUT_DIR/$i push origin release/$VERSION
+        git -C $CHECKOUT_DIR/$i push origin v$VERSION
+    done
+fi
 
 # Create source distributions
 for i in $REPOS; do
