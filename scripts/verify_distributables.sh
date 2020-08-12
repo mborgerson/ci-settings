b6015526cba669608615eba9d39d51bb421f35a7
Kevin Phoenix <kevin@kphoenix.us>
2020-08-12 15:11:50 -0700

    Add more display names; refactor sdist verification

--- scripts/verify_distributables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-python -m venv angr_venv
-source angr_venv/bin/activate || source angr_venv/Scripts/activate
-
-pip install sdist/*
-
-python -c "import angr; print('angr imports!')"
