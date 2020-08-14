18fa674321d72e6b64f082434c28be8dd26c7f98
Kevin Phoenix <kevin@kphoenix.us>
2020-08-13 19:30:04 -0700

    Fix sed string

--- scripts/prepare_venv_from_sdist.sh
+++ scripts/prepare_venv_from_sdist.sh
@@ -4,7 +4,7 @@ PYTHON=$1
 SDIST_LOC=$2
 VENV_PATH=$3
 
-PACKAGES=$($PYTHON scripts/get_repo_names.py --python-only | sed -E "s/([[:alnum:]-]+)/$VENV_LOC\/\1*/g")
+PACKAGES=$($PYTHON scripts/get_repo_names.py --python-only | sed -E "s/([[:alnum:]-]+)/$SDIST_LOC\/\1*/g")
 $PYTHON -m venv $VENV_PATH
 source $VENV_PATH/bin/activate || source $VENV_PATH/Scripts/activate
 pip install wheel
