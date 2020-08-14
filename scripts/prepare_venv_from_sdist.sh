13b7d4e9e18310e040f217634d9bb768f4d27586
Kevin Phoenix <kevin@kphoenix.us>
2020-08-13 20:46:41 -0700

    Upgrade pip in wheels venv

--- scripts/prepare_venv_from_sdist.sh
+++ scripts/prepare_venv_from_sdist.sh
@@ -7,5 +7,5 @@ VENV_PATH=$3
 PACKAGES=$($PYTHON scripts/get_repo_names.py --python-only | sed -E "s/([[:alnum:]-]+)/$SDIST_LOC\/\1*/g")
 $PYTHON -m venv $VENV_PATH
 source $VENV_PATH/bin/activate || source $VENV_PATH/Scripts/activate
-pip install wheel
+pip install --upgrade pip wheel
 pip install $PACKAGES
