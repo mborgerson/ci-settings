de25874e5119d2030786f86864af0aea846b01e0
Kevin Phoenix <kevin@kphoenix.us>
2020-08-14 16:02:26 -0700

    Avoid abusing relative paths in build_wheels.sh

--- scripts/build_wheels.sh
+++ scripts/build_wheels.sh
@@ -1,17 +1,22 @@
 set -ex
 
 PYTHON=$1
-SDIST_LOC=$2
-VENV_PATH=$3
+SDIST_LOC=$(realpath $2)
+VENV_PATH=$(realpath $3)
 
 PACKAGES=$($PYTHON scripts/get_repo_names.py --python-only)
 source $VENV_PATH/bin/activate || source $VENV_PATH/Scripts/activate
 
-mkdir -p wheels
+WHEELS=$(realpath wheels)
+mkdir -p $WHEELS wheels_build
+pushd wheels_build
+
 for package in ${PACKAGES[@]}; do
     tar -xf $SDIST_LOC/$package*
-    pushd dist/$package
+    pushd $package*
     python setup.py bdist_wheel
-    mv dist/* ../../wheels
+    mv dist/* $WHEELS
     popd
 done
+
+popd
