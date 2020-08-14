4bab64497c4af25ac36df3d51734c0ace50a5f4b
Kevin Phoenix <kevin@kphoenix.us>
2020-08-14 13:33:36 -0700

    Explicitly use venv pip in prepare_venv_from_sdist.sh

--- scripts/prepare_venv_from_sdist.sh
+++ scripts/prepare_venv_from_sdist.sh
@@ -7,5 +7,5 @@ VENV_PATH=$3
 PACKAGES=$($PYTHON scripts/get_repo_names.py --python-only | sed -E "s/([[:alnum:]-]+)/$SDIST_LOC\/\1*/g")
 $PYTHON -m venv $VENV_PATH
 source $VENV_PATH/bin/activate &> /dev/null || source $VENV_PATH/Scripts/activate
-pip install --upgrade pip wheel
-pip install $PACKAGES
+python -m pip install --upgrade pip wheel
+python -m pip install $PACKAGES
