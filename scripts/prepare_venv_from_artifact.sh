ab07c32f473229074a6cd5ef6add80f006b6dc74
Kevin Phoenix <kevin@kphoenix.us>
2020-08-19 11:59:48 -0700

    Try to split string into array

--- scripts/prepare_venv_from_artifact.sh
+++ scripts/prepare_venv_from_artifact.sh
@@ -4,8 +4,8 @@ PYTHON=$1
 SDIST_LOC=$2
 VENV_PATH=$3
 
-PACKAGES=$($PYTHON scripts/get_repo_names.py --python-only | sed -E "s/([[:alnum:]-]+)/$SDIST_LOC\/\1*/g")
+PACKAGES=($($PYTHON scripts/get_repo_names.py --python-only | sed -E "s#([[:alnum:]-]+)#$SDIST_LOC/\1*#g"))
 $PYTHON -m venv $VENV_PATH
 source $VENV_PATH/bin/activate &> /dev/null || source $VENV_PATH/Scripts/activate
 python -m pip install --upgrade pip wheel
-python -m pip install $PACKAGES
+python -m pip install "${PACKAGES[@]}"
