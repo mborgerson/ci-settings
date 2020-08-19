91046f32ceeadb5b5067e0ed3ecd39e967fa4c1e
Kevin Phoenix <kevin@kphoenix.us>
2020-08-19 13:00:10 -0700

    Keep fiddling with sed string

--- scripts/prepare_venv_from_artifact.sh
+++ scripts/prepare_venv_from_artifact.sh
@@ -4,7 +4,7 @@ python=$1
 sdist_path=$2
 venv_path=$3
 
-packages=($($python scripts/get_repo_names.py --python-only | sed -E "s#-#_#g s#([[:alnum:]_]+)#$sdist_path/\1*#g"))
+packages=($($python scripts/get_repo_names.py --python-only | sed -E "s#-#*#g; s#([[:alnum:]*]+)#$sdist_path/\1*#g"))
 $python -m venv $venv_path
 source $venv_path/bin/activate &> /dev/null || source $venv_path/Scripts/activate
 python -m pip install --upgrade pip wheel
