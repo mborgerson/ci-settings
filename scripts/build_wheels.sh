aac57f2a78a59c3e0b2018e1b13aa87162f5b723
Kevin Phoenix <kevin@kphoenix.us>
2020-08-19 12:35:05 -0700

    Use lowercase for local variable names

--- scripts/build_wheels.sh
+++ scripts/build_wheels.sh
@@ -5,22 +5,22 @@ function realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
-PYTHON=$1
-SDIST_LOC=$(realpath $2)
-VENV_PATH=$(realpath $3)
+python=$1
+sdist_path=$(realpath $2)
+venv_path=$(realpath $3)
 
-PACKAGES=$($PYTHON scripts/get_repo_names.py --python-only)
-source $VENV_PATH/bin/activate || source $VENV_PATH/Scripts/activate
+packages=$($python scripts/get_repo_names.py --python-only)
+source $venv_path/bin/activate &> /dev/null || source $venv_path/Scripts/activate
 
-WHEELS=$(realpath wheels)
-mkdir -p $WHEELS wheels_build
+wheels=$(realpath wheels)
+mkdir -p $wheels wheels_build
 pushd wheels_build
 
-for package in ${PACKAGES[@]}; do
-    tar -xf $SDIST_LOC/$package-8*
-    pushd $package-8*
+for package in ${packages[@]}; do
+    tar -xf $sdist_path/$package-$VERSION_MAJOR*
+    pushd $package-$VERSION_MAJOR*
     python setup.py bdist_wheel
-    mv dist/* $WHEELS
+    mv dist/* $wheels
     popd
 done
 
