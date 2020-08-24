229f661e6939adcdbb8dff168b640ac3df039486
Kevin Phoenix <kevin@kphoenix.us>
2020-08-24 14:58:26 -0700

    Try again to fix wheels building

--- scripts/prepare_venv_from_artifact.sh
+++ scripts/prepare_venv_from_artifact.sh
@@ -1,10 +1,11 @@
+source vars.sh
 set -ex
 
 python=$1
 sdist_path=$2
 venv_path=$3
 
-packages=($($python scripts/get_repo_names.py --python-only | sed -E "s#-#*#g; s#([[:alnum:]*]+)#$sdist_path/\1*#g"))
+packages=($($python scripts/get_repo_names.py --python-only | sed -E "s#([[:alnum:]*]+)#$sdist_path/\1-$VERSION*#g"))
 $python -m venv $venv_path
 source $venv_path/bin/activate &> /dev/null || source $venv_path/Scripts/activate
 python -m pip install --upgrade pip wheel
