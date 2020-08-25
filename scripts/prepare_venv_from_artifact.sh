d6b66294bd0e8b6656b660cdf261dded8c7aebc2
Kevin Phoenix <kevin@kphoenix.us>
2020-08-24 21:45:53 -0700

    Try and work around macos sed issues

--- scripts/prepare_venv_from_artifact.sh
+++ scripts/prepare_venv_from_artifact.sh
@@ -5,7 +5,7 @@ python=$1
 sdist_path=$2
 venv_path=$3
 
-packages=$($python scripts/get_repo_names.py --python-only | sed -E "s#[_-]#*#g; s#(\\S+)#$sdist_path/\1-$VERSION*#g")
+packages=$($python scripts/get_repo_names.py --python-only | sed -E "s#[_-]#*#g; s#(\\S\\S*)#$sdist_path/\1-$VERSION*#g")
 $python -m venv $venv_path
 source $venv_path/bin/activate &> /dev/null || source $venv_path/Scripts/activate
 python -m pip install --upgrade pip wheel
