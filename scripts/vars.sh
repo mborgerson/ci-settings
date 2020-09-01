02f93708de5aa545a715a80d8efc1a6908b45c31
Kevin Phoenix <kevin@kphoenix.us>
2020-08-31 18:25:34 -0700

    Update versioning

--- scripts/vars.sh
+++ scripts/vars.sh
@@ -1,5 +1,6 @@
-VERSION_MAJOR=8
-VERSION=$VERSION_MAJOR$(date +.%y.%m.%d | sed -e "s/\.0*/./g")
-VERSION_TUPLE=$(awk -F '.' '{ printf "(%d, %d, %d, %d)\n", $1, $2, $3, $4}' <<< $VERSION)
+VERSION_MAJOR=9
+VERSION_MINOR=0
+VERSION=$VERSION_MAJOR.$VERSION_MINOR.$BUILD_BUILDID
+VERSION_TUPLE=$(awk -F '.' '{ printf "(%d, %d, %d)\n", $1, $2, $3}' <<< $VERSION)
 REPOS=$($python scripts/get_repo_names.py)
 CHECKOUT_DIR=repos
